dd0b01b915a888c01de6064cd49ca54692ca66c7
hmalabehqrf <hmalabeh@qrf.org>
2025-07-10 14:59:20 +0300

    python_and_package_upgrade

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     ],
     install_requires=[
         "Django>=5.0.14",
-        "requests>=2.31.0",
+        "requests>=2.32.4",
         "urllib3>=1.26.15",
         "elasticsearch>=7.0.0",
         "elasticsearch_dsl>=7.4.1",
